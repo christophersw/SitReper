892e42ad895b299ed579571ea06c282abcd33657
Chris Webster <christophersw@gmail.com>
2015-03-12 01:28:58 +0000

    Added passport library & config file.

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
-var express = require('express'), 
-    app = express();
+var express = require('express') 
+  , config = require('.//config.js')
+  , app = express();
+
+
+//Configuire Authentication 
+var passport = require('passport')
+  , GoogleStrategy = require('passport-google').Strategy;
+
+passport.use(new GoogleStrategy({
+    returnURL: config.url + '/auth/google/return',
+    realm: config.url
+  },
+  function(identifier, profile, done) {
+    User.findOrCreate({ openId: identifier }, function(err, user) {
+      done(err, user);
+    });
+  }
+));
+
+// Redirect the user to Google for authentication.  When complete, Google
+// will redirect the user back to the application at
+//     /auth/google/return
+app.get('/auth/google', passport.authenticate('google'));
+
+// Google will redirect the user to this URL after authentication.  Finish
+// the process by verifying the assertion.  If valid, the user will be
+// logged in.  Otherwise, authentication has failed.
+app.get('/auth/google/return', 
+  passport.authenticate('google', { successRedirect: '/',
+                                    failureRedirect: '/login' }));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World!' + 
+            '<a href="/auth/google">Sign In with Google</a>');
 });
 
 var server = app.listen(process.env.PORT, function () {
---
+++ config.js
@@ -0,0 +1,8 @@
+var config;
+
+config = {
+    url : 'https://sitreper-christophersw.c9.io'
+};
+
+// Export config
+module.exports = config;
