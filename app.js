f532901661c99423cd2db3d510f682c5f009be10
Chris Webster <christophersw@gmail.com>
2015-03-12 02:20:57 +0000

    cleanup.

--- app.js
+++ app.js
@@ -3,36 +3,8 @@ var express = require('express')
   , app = express();
 
 
-//Configuire Authentication 
-var passport = require('passport')
-  , GoogleStrategy = require('passport-google').Strategy;
-
-passport.use(new GoogleStrategy({
-    returnURL: config.url + '/auth/google/return',
-    realm: config.url
-  },
-  function(identifier, profile, done) {
-    User.findOrCreate({ openId: identifier }, function(err, user) {
-      done(err, user);
-    });
-  }
-));
-
-// Redirect the user to Google for authentication.  When complete, Google
-// will redirect the user back to the application at
-//     /auth/google/return
-app.get('/auth/google', passport.authenticate('google'));
-
-// Google will redirect the user to this URL after authentication.  Finish
-// the process by verifying the assertion.  If valid, the user will be
-// logged in.  Otherwise, authentication has failed.
-app.get('/auth/google/return', 
-  passport.authenticate('google', { successRedirect: '/',
-                                    failureRedirect: '/login' }));
-
 app.get('/', function (req, res) {
-  res.send('Hello World!' + 
-            '<a href="/auth/google">Sign In with Google</a>');
+  res.send('Hello World!');
 });
 
 var server = app.listen(process.env.PORT, function () {
@@ -40,6 +12,6 @@ var server = app.listen(process.env.PORT, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('App listening at: ', host, port);
 
 });
